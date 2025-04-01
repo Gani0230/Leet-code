24ab70bb65eb7c6b58fa6daaaec18f4cccfe6d1e
Gani0230 <ganeshjadhav7478@gmail.com>
2025-04-01 20:53:02 +0530

    solve grouped anagram problem using dict Hint- sort eact word

---
+++ Array and Hashing/easy/grouped_anagram.py
@@ -0,0 +1,41 @@
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Explanation:
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+# Example 2:
+# Input: strs = [""]
+# Output: [[""]]
+
+# Example 3:
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+# Constraints:
+# 1 <= strs.length <= 104
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagramDict = {}
+
+        for i in strs:
+            sorted_strs = "".join(sorted(i))
+            if (sorted_strs in anagramDict):
+                anagramDict[sorted_strs].append(i)
+            else:
+                anagramDict[sorted_strs] = [i]
+        return list(anagramDict.values())
+        
+s = Solution()
+print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(s.groupAnagrams([""]))
+print(s.groupAnagrams(["a"]))
+
+
+
